6038ce819e64caa51bc8a4ded3c6a2d8a125ce2e
Omar <omar0940@gmail.com>
2021-04-28 15:55:58 +0200

    password gen

--- src/index.js
+++ src/index.js
@@ -6,93 +6,51 @@
 // passwordGen()  ➞ Oy$tkBGoo5fg§mO4AdV&Kh8XXZR4d7K
 // passwordGen()  ➞ v0ZwDrn@hfI€kOKWH§k6kqub6zfpuyP
 
-const passwordGen = () => {
-  let characters = [
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    6,
-    7,
-    8,
-    9,
-    "!",
-    "§",
-    "$",
-    "%",
-    "&",
-    "?",
-    "@",
-    "€",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  let result = "";
-  let randomNum;
-
-  for (let i = 0; i <= 30; i++) {
-    randomNum = Math.floor(Math.random() * characters.length);
-    // result = result + characters[randomNum];
-    result += characters[randomNum];
-  }
-  return result;
-};
-
-// console.log(passwordGen());
-// other way
-let numbers = [0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9];
-let alpha = [
+let characters = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  6,
+  7,
+  8,
+  9,
+  "!",
+  "§",
+  "$",
+  "%",
+  "&",
+  "?",
+  "@",
+  "€",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
   "a",
   "b",
   "c",
@@ -119,26 +77,86 @@ let alpha = [
   "y",
   "z",
 ];
-let charactersSp = ["!", "§", "$", "%", "&", "?", "@", "€"];
-const passGen = () => {
+
+const characters2 = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  6,
+  7,
+  8,
+  9,
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const passwordGen = (arr) => {
   let result = "";
-  const randomNumGetter = (arr) => {
-    return Math.floor(Math.random() * arr.length);
-  };
-  for (let i = 0; i <= 17; i++) {
-    if (i % 2 == 0) {
-      result += numbers[randomNumGetter(numbers)];
-    }
-    if (i % 3 == 0) {
-      result += alpha[randomNumGetter(alpha)].toUpperCase();
-    } else {
-      result += alpha[randomNumGetter(alpha)];
-    }
-    if (i % 5 == 0) {
-      result += charactersSp[randomNumGetter(charactersSp)];
-    }
+  let randomNum;
+  let num = document.querySelector("#num").value;
+  for (let i = 0; i < num; i++) {
+    randomNum = Math.floor(Math.random() * arr.length);
+    // result = result + characters[randomNum];
+    result += arr[randomNum];
   }
-  return result;
+  return (document.querySelector("#password").innerHTML = result);
 };
 
-console.log(passGen());
+document
+  .querySelector("#char")
+  .addEventListener("click", () => passwordGen(characters2));
+document
+  .querySelector("#btn")
+  .addEventListener("click", () => passwordGen(characters));
