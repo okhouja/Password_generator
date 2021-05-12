c1c5f9e30626509c34b5254e2e8c999c12b5f2ff
Omar <omar0940@gmail.com>
2021-05-12 16:06:06 +0200

    password gen

--- src/index.js
+++ src/index.js
@@ -5,12 +5,26 @@
 // passwordGen()  ➞ Y3mI9x7bvPY86T?oM§hIbgh!MLwA77N
 // passwordGen()  ➞ Oy$tkBGoo5fg§mO4AdV&Kh8XXZR4d7K
 // passwordGen()  ➞ v0ZwDrn@hfI€kOKWH§k6kqub6zfpuyP
+let elem = document.querySelector('input[type="range"]');
+
+let rangeValue = function () {
+  let newValue = elem.value;
+  let target = document.querySelector(".value");
+  target.innerHTML = newValue;
+};
+
+elem.addEventListener("input", rangeValue);
 
 let characters =
   "0123456789ABCDEFGHIKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz!@#$%^&*()_=+*/€><{}[]:;.,";
 
 const characters2 =
   "0123456789ABCDEFGHIKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz";
+
+const numbers = "0123456789";
+const symbol = "!@#$%^&*()_=+*/€><{}[]:;.,";
+const lowerCase = "abcdefghiklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
 
 const passwordGen = (arr) => {
   let result = "";
@@ -24,9 +38,25 @@ const passwordGen = (arr) => {
   return (document.querySelector("#password").innerHTML = result);
 };
 
-document
-  .querySelector("#char")
-  .addEventListener("click", () => passwordGen(characters2));
+// document
+//   .querySelector("#char")
+//   .addEventListener("click", () => passwordGen(characters2));
 document
   .querySelector("#btn")
   .addEventListener("click", () => passwordGen(characters));
+
+document
+  .querySelector("#symbol")
+  .addEventListener("click", () => passwordGen(symbol));
+
+document
+  .querySelector("#number")
+  .addEventListener("click", () => passwordGen(numbers));
+
+document
+  .querySelector("#lowercase")
+  .addEventListener("click", () => passwordGen(lowerCase));
+
+document
+  .querySelector("#uppercase")
+  .addEventListener("click", () => passwordGen(upperCase));
