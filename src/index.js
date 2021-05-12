ead609eb47df0376c6664940d6caccc7d0b2b97d
Omar <omar0940@gmail.com>
2021-05-12 16:12:25 +0200

    password gen

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-// Create a program that generate a random password with the length of 30 character, every time you call the function should give you a different password
-
-// Examples you will have other passwords for sure
-
-// passwordGen()  ➞ Y3mI9x7bvPY86T?oM§hIbgh!MLwA77N
-// passwordGen()  ➞ Oy$tkBGoo5fg§mO4AdV&Kh8XXZR4d7K
-// passwordGen()  ➞ v0ZwDrn@hfI€kOKWH§k6kqub6zfpuyP
 let elem = document.querySelector('input[type="range"]');
 
 let rangeValue = function () {
